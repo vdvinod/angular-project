445882b5e655eac14902dbbd8956ead7ca3d9d75
vinod singh rawat <vdvinodrawat@gmail.com>
2020-02-09 23:09:06 +0530

    first commit created folder structure

---
+++ src/app/material.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material';
+
+@NgModule({
+    exports: [MatButtonModule],
+    imports: [MatButtonModule]
+})
+export class MaterialModule {}
